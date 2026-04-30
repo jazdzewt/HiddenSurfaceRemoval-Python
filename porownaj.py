70c8a256992c55149f7c4135850483abdd56b884
jazdzewt <tymon.jazdzewski@pw.edu.pl>
2026-04-30 12:11:51 +0200

    Nie dziala

--- porownaj.py
+++ porownaj.py
@@ -13,7 +13,6 @@ def wektor_normalny(wielokat):
         wektor1 = drugi_punkt - pierwszy_punkt
         wektor2 = trzeci_punkt - pierwszy_punkt
         
-        # iloczyn wektorowy 
         normalna = np.cross(wektor1, wektor2)
         dlugosc = np.linalg.norm(normalna)
         
@@ -39,7 +38,7 @@ def czy_z_tylu(wielokat, normalna, odleglosc):
         if wynik >= e:
             return False
     return True
-'''
+
 def czy_z_przodu(wielokat, normalna, odleglosc):
     for wierzcholek in wielokat:
         punkt = wierzcholek[:3]
@@ -47,26 +46,56 @@ def czy_z_przodu(wielokat, normalna, odleglosc):
         if wynik <= -e:
             return False
     return True
-'''
+
 
 def sprawdz_odleglosc(sciana):
 
     wierzcholki = sciana[0]
 
-    suma = 0 
+    max_Z = 0 
 
+    # Centroid Z (srednia) - stabilniejszy niz max_Z
     for w in wierzcholki:
+        if w[2] > max_Z:
+            max_Z = w[2]
 
-        suma += w[2]
+    return max_Z
+
+def sciany_nakladaja_sie_na_ekranie(sciana1, sciana2):
+    # Najpierw znajdujemy skrajne punkty pierwszej ściany na płaskim ekranie
+    x1 = []
+    y1 = []
+
+    for wierzcholek in sciana1:
+
+        z = abs(wierzcholek[2]) + e
+        x1.append(wierzcholek[0] / z)
+        y1.append(wierzcholek[1] / z)
+        
+    min_x1, max_x1 = min(x1), max(x1)
+    min_y1, max_y1 = min(y1), max(y1)
+
+    # To samo robimy dla drugiej ściany
+    x2 = []
+    y2 = []
+    for wierzcholek in sciana2:
+
+        z = abs(wierzcholek[2]) + e
+        x2.append(wierzcholek[0] / z)
+        y2.append(wierzcholek[1] / z)
+        
+    min_x2, max_x2 = min(x2), max(x2)
+    min_y2, max_y2 = min(y2), max(y2)
     
-    odleglosc = suma / len(wierzcholki)
-
-    return odleglosc
+    # Sprawdzamy czy narysowane prostokąty się mijają (nie dotykają się)
+    if max_x1 < min_x2 or max_x2 < min_x1 or max_y1 < min_y2 or max_y2 < min_y1:
+        return False # Mijają się, więc nie musimy ich sortować względem siebie
+    else:
+        return True # Nakładają się na ekranie
 
 
 def sortuj_sciany(sciany):
 
-    # Wstepne sortowanie: od najdalszych (duze srednie Z) do najblizszych
     sciany = sorted(sciany, key=sprawdz_odleglosc, reverse=True)
 
     
@@ -75,20 +104,23 @@ def sortuj_sciany(sciany):
             sciana1 = sciany[i][0]
             sciana2 = sciany[j][0]
 
-            #normalna1, odleglosc1 = wektor_normalny(sciana1)
+            normalna1, odleglosc1 = wektor_normalny(sciana1)
             normalna2, odleglosc2 = wektor_normalny(sciana2)
 
-            if czy_z_tylu(sciana1, normalna2, odleglosc2):
+            #if sciany_nakladaja_sie_na_ekranie(sciana1, sciana2):
+
+                # Zamień tylko gdy OBE płaszczyzny jednoznacznie potwierdzają kolejność
+                # (and = konserwatywne, or byłoby zbyt agresywne i powodowało błędne zamiany)
+            if czy_z_tylu(sciana2, normalna1, odleglosc1) and czy_z_przodu(sciana1, normalna2, odleglosc2):
                 temp = sciany[i] 
                 sciany[i] = sciany[j]
-                sciany[j] = temp 
-            '''
-            elif czy_z_przodu(sciana2, normalna1, odleglosc1):
-                temp = sciany[i] 
-                sciany[i] = sciany[j]
-                sciany[j] = temp 
-            '''
-    
-    sciany.reverse()
+                sciany[j] = temp                
+                '''
+                elif czy_z_przodu(sciana1, normalna2, odleglosc2):
+                    temp = sciany[i] 
+                    sciany[i] = sciany[j]
+                    sciany[j] = temp
+                '''
+    #sciany.reverse()
 
     return sciany
